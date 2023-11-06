43710741e62801c54b51591c7fabd0f0b0e1107b
Transurgeon <peter.zijie@gmail.com>
2023-11-05 19:03:19 -0500

    adding first set of unit tests:

---
+++ test/com/shashi/beans/OrderDetailsTest.java
@@ -0,0 +1,72 @@
+package com.shashi.beans;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderDetailsTest {
+
+    @Test
+    void getOrderId() {
+    }
+
+    @Test
+    void setOrderId() {
+    }
+
+    @Test
+    void getProdName() {
+    }
+
+    @Test
+    void setProdName() {
+    }
+
+    @Test
+    void getQty() {
+    }
+
+    @Test
+    void setQty() {
+    }
+
+    @Test
+    void getAmount() {
+    }
+
+    @Test
+    void setAmount() {
+    }
+
+    @Test
+    void getTime() {
+    }
+
+    @Test
+    void setTime() {
+    }
+
+    @Test
+    void getProdImage() {
+    }
+
+    @Test
+    void setProdImage() {
+    }
+
+    @Test
+    void getProductId() {
+    }
+
+    @Test
+    void setProductId() {
+    }
+
+    @Test
+    void getShipped() {
+    }
+
+    @Test
+    void setShipped() {
+    }
+}
