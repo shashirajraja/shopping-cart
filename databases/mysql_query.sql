52d5a7ffdf3c5e6e1c1861d9ececedf6d46ea7a5
WenbooSu <105067670+WenbooSu@users.noreply.github.com>
2023-11-29 14:33:13 -0500

    Update mysql_query.sql

--- databases/mysql_query.sql
+++ databases/mysql_query.sql
@@ -265,3 +265,33 @@ INSERT INTO `shopping-cart`.`usercart` (`username`, `prodid`, `quantity`) VALUES
 
 COMMIT;
 
+  -- -----------------------------------------------------
+-- Data for table `shopping-cart`.`product` with condition column
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `shopping-cart`;
+ALTER TABLE `shopping-cart`.`product`
+ADD  `condition` ENUM('new', 'used') DEFAULT NULL;
+
+COMMIT;
+
+-- -----------------------------------------------------
+-- Data for table `shopping-cart`.`product` with sold column
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `shopping-cart`;
+ALTER TABLE `shopping-cart`.`product`
+ADD  `sold` int DEFAULT NULL;
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `shopping-cart`.`product` with discount column
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `shopping-cart`;
+ALTER TABLE `shopping-cart`.`product`
+ADD  `discount` int DEFAULT NULL;
+
+COMMIT;
