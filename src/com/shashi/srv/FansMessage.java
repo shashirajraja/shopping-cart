d7b567a51b8d439d5314dceca4390549fc2c5ab1
Yu Ying Li Wen <usbthekey@gmail.com>
2023-11-29 15:57:58 -0500

    Name change: Ellison Electro >> Concordia

--- src/com/shashi/srv/FansMessage.java
+++ src/com/shashi/srv/FansMessage.java
@@ -28,12 +28,12 @@ public class FansMessage extends HttpServlet {
 
 		response.setContentType("text/html");
 		String htmlTextMessage = "" + "<html>" + "<body>"
-				+ "<h2 style='color:green;'>Message to Ellison Electronics</h2>" + ""
+				+ "<h2 style='color:green;'>Message to Concordia Shopping Cart App</h2>" + ""
 				+ "Fans Message Received !!<br/><br/> Name: " + name + "," + "<br/><br/> Email Id: " + email
 				+ "<br><br/>" + "Comment: " + "<span style='color:grey;'>" + comments + "</span>"
 				+ "<br/><br/>We are glad that fans are choosing us! <br/><br/>Thanks & Regards<br/><br/>Auto Generated Mail"
 				+ "</body>" + "</html>";
-		String message = MailMessage.sendMessage("ellison.alumni@gmail.com", "Fans Message | " + name + " | " + email,
+		String message = MailMessage.sendMessage("concordiashoppingcartapp@gmail.com", "Fans Message | " + name + " | " + email,
 				htmlTextMessage);
 		if ("SUCCESS".equals(message)) {
 			message = "Comments Sent Successfully";
