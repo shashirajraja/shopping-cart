204ccdffaa620920e9ab9a6616f77e34a5df8f98
MissMystie <miss_mystie@outlook.com>
2023-11-29 20:56:01 -0500

    DiscountServiceImpl minor update

--- src/com/shashi/service/impl/DiscountServiceImpl.java
+++ src/com/shashi/service/impl/DiscountServiceImpl.java
@@ -8,37 +8,37 @@ public class DiscountServiceImpl implements DiscountService {
 	@Override
 	public void getBestSellingByInterest() {
 
-		
+		//TODO ?
 	}
 
 	@Override
 	public void checkInStock(ProductBean product) {
 
-		
+		//TODO ?
 	}
 
 	@Override
 	public void addDiscount(ProductBean product, int discountAmount) {
 
-		
+		product.setCurrentDiscount(discountAmount);
 	}
 
 	@Override
 	public void removeDiscount(ProductBean product) {
 
-		
+		product.setCurrentDiscount(0);
 	}
 
 	@Override
 	public void displayDiscount(ProductBean product) {
 
-		
+		//TODO ?
 	}
 
 	@Override
 	public void displayAdminSuggestions() {
 
-		
+		//TODO ?
 	}
 
 }
